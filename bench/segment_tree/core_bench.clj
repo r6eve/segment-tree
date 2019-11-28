48ada137f2205a69e2b5487d7ab21d6dca441a30
r6eve <r6eve179f@gmail.com>
2019-11-28 14:14:17 +0900

    Add LICENSE

--- bench/segment_tree/core_bench.clj
+++ bench/segment_tree/core_bench.clj
@@ -1,3 +1,10 @@
+;;;;
+;;;;           Copyright r6eve 2019 -
+;;;;  Distributed under the Boost Software License, Version 1.0.
+;;;;     (See accompanying file LICENSE_1_0.txt or copy at
+;;;;           https://www.boost.org/LICENSE_1_0.txt)
+;;;;
+
 (ns segment-tree.core-bench
   (:require [criterium.core :as criterium]
             [segment-tree.core :as segment-tree]))
