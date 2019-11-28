f1e75089a9f1951ae1e7f8f8c3cd3d811b31f813
r6eve <r6eve179f@gmail.com>
2019-11-28 14:17:25 +0900

    Fix benchmark

--- bench/segment_tree/core_bench.clj
+++ bench/segment_tree/core_bench.clj
@@ -9,17 +9,21 @@
   (:require [criterium.core :as criterium]
             [segment-tree.core :as segment-tree]))
 
-(let [n 10 s (segment-tree/make n 2147483647)]
+(let [n 10]
   (criterium/with-progress-reporting
     (criterium/quick-bench
      (do
-       (doseq [[i x] (map-indexed vector (range 50))]
-         (segment-tree/update! s i x))
+       (let [s (segment-tree/make n 2147483647)]
+         (doseq [[i x] (map-indexed vector (range 50))]
+           (segment-tree/update! s i x)))
        nil)
      :verbose))
-  (criterium/with-progress-reporting
-    (criterium/quick-bench
-     (do
-       (segment-tree/find-min s 0 (dec n))
-       nil)
-     :verbose)))
+  (let [s (segment-tree/make n 2147483647)]
+    (doseq [[i x] (map-indexed vector (range 50))]
+      (segment-tree/update! s i x))
+    (criterium/with-progress-reporting
+      (criterium/quick-bench
+       (do
+         (segment-tree/find-min s 0 (dec n))
+         nil)
+       :verbose))))
